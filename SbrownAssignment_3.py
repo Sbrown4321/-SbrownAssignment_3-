4710015343497eac0a799a7c3499a4defd227a29
Steven Brown <sbrow165@msudenver.edu>
2016-03-13 08:15:40 -0600

    Change pass arguments to working function

--- SbrownAssignment_3.py
+++ SbrownAssignment_3.py
@@ -25,6 +25,7 @@ After 0, "zero" 1, "one" 10, "ten" have been inserted, it will be:
 class my_hash_set:
     def __init__(self, init=None):
         self.__limit = 10
+        self.__none = None
         self.__items = [None] * self.__limit
         self.__count = 0
 
@@ -34,19 +35,26 @@ class my_hash_set:
 
     def __len__(self): return(self.__count)
 
+    #Add to dictionary of class not object
+    def __add(self, items):
+        h = self.__hash(items)
+        self.__items[h].append(items)
+        if (0.0 + self.__count) / self.__limit\
+            > .75: self.__rehash()
+
     def __flattened(self):
-        flattened = filter(lambda x:x != self.__none, self.__items)
+        flattened = filter(lambda x: x != None, self.__items)
         flattened = [item for inner in flattened for item in inner]
-        return (flattened)
+        return(flattened)
 
     def __iter__(self): return(iter(self.__flattened()))
-    def __str__(self): return (str(self.__flattened()))
+    def __str__(self): return(str(self.__flattened()))
 
     def __setitem__(self, key, value):
         h = hash(key) % self.__limit
 
-        if not value[h]:
-            self.__items[h] = ([key, value])
+        if not self.__items[h]:
+            self.__items[h] = [[key, value]]
         else:
             self.__items[h].append([key, value])
 
@@ -54,28 +62,56 @@ class my_hash_set:
 
         if (0.0 + self.__count) / self.__limit > .75: self.__rehash()
 
-    def __rehash(self):
-        pass
+    def __hash(self, item): return(hash(item) % self.__limit)
 
+    def __rehash(self):
+        #if trace: print("rehasing before:", self.__items)
+        value = self.__flattened()
+
+        self.__limit *= 2
+        self.__items =[self.__none] * self.__limit
+
+        for i in value: self.__add(i)
+
+        #if trace: print("rehashing before:", self.__items)
+
+    #Need false statement variable rechecked
     def __contains__(self, key):
-        pass
+        h = self.__hash(key)
+        #if  self.__items[h] != self.__none:
+        for i in self.__items[h]:
+            if i == key: return True
+        return False
 
     def __getitem__(self, key):
-         pass
+
+        for i in key:
+            self.__setitem__(i)
+        return(self)
 
     def __delitem__(self, key):
-        pass
+        if key not in self: raise(KeyError(key))
+
+        h = self.__hash(key)
+        self.__items[h].remove(key)
+
+        # not strictly necessary, but for consistency
+        if self.__items[h] == []:
+            self.__items[h] = self.__none
+
+        self.__count -= 1
 
 
 class test_my_hash_set(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(len(my_hash_set()), 0)
-
+    def test_len_one(self):
+        s = my_hash_set()
+        s[1] = "one"
+        self.assertEquals(len(s), 1)
     def test_add_one(self):
         s = my_hash_set()
         s[1] = "one"
-        s[2] = "two"
-        self.assertEquals(len(s), 1)
         self.assertEquals(s[1], "one")
     def test_add_two(self):
         s = my_hash_set()
@@ -86,7 +122,7 @@ class test_my_hash_set(unittest.TestCase):
         s = my_hash_set()
         s[1] = "one"
         s[1] = "one"
-        self.assetEquals(len(s), 1)
+        self.assertEquals(len(s), 1)
 
 
 if __name__ == '__main__':
